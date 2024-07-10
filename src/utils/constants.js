4d3c5e2b163601c5f5304fdc32475f9bfe871a21
Anurag Pandey <77746382+anurag2909@users.noreply.github.com>
2024-07-10 23:35:18 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
-const GOOGLE_API_KEY = "AIzaSyB2ElTHgcggCGJLoC31X9lk6pXy6MUc6S8";
 
-=======
->>>>>>> 6fe025a79793e2a4eda62181bf0952b6401eef12
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
